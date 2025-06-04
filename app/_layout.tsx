9d6c403f8ac992a4208125311a17dccffb3f2293
Giang Huynh <trgianghuynhdev1808@gmail.com>
2025-06-04 17:01:47 +0700

    init: :sparkles: Authentication flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,32 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+import { SessionProvider, useSession } from "../ctx";
+import { SplashScreenController } from "../splash";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+export default function Root() {
+  // Set up the auth context and render our layout inside of it.
+  return (
+    <SessionProvider>
+      <SplashScreenController />
+      <RootNavigator />
+    </SessionProvider>
+  );
+}
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+// Separate this into a new component so it can access the SessionProvider context later
+function RootNavigator() {
+  const { session } = useSession();
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  console.log("session", session);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Protected guard={Boolean(session)}>
+        <Stack.Screen name="(app)" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!Boolean(session)}>
+        <Stack.Screen name="sign-in" />
+      </Stack.Protected>
+    </Stack>
   );
 }
